219909887753943312fa2ab443bc8c6ef0adc3ef
eduardohki <eh+github@runlevel0.me>
2024-09-04 19:22:50 +0200

    chore(iac): document custom stack param and deletionProtection setting in  the table

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AttributeType, Billing, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 interface AppSyncStackProps extends StackProps {
+  /** Defines the removal policy applied to stateful resources in the Stack.
+   *
+   * Useful for preventing orphaned stateful resources upon Stack deletion in non-prod environments.
+   */
   removalPolicy: RemovalPolicy;
 }
 
@@ -15,6 +19,7 @@ export class AppSyncStack extends Stack {
       partitionKey: { name: 'PK', type: AttributeType.STRING },
       sortKey: { name: 'SK', type: AttributeType.STRING },
       removalPolicy: props.removalPolicy,
+      // prevent accidental table deletion when the data is to be retained
       deletionProtection: props.removalPolicy === RemovalPolicy.DESTROY ? false : true,
     });
   }
