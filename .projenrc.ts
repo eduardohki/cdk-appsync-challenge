0246b026620f531ec31d111a3c17137fbb224093
eduardohki <eh+github@runlevel0.me>
2024-09-04 18:07:44 +0200

    add initial projen config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,14 @@
 import { DeployableAwsCdkTypeScriptApp } from 'deployable-awscdk-app-ts';
 import { javascript } from 'projen';
 const project = new DeployableAwsCdkTypeScriptApp({
-  cdkVersion: '2.155.0',
-  defaultReleaseBranch: 'main',
-  devDeps: ['deployable-awscdk-app-ts'],
   name: 'cdk-appsync-challenge',
+  description: 'CDK AppSync API Challenge',
   packageManager: javascript.NodePackageManager.PNPM,
+  defaultReleaseBranch: 'main',
+  cdkVersion: '2.155.0',
+  deps: [],
+  devDeps: ['deployable-awscdk-app-ts'],
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
